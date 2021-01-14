db72e86d24adf268498836ce7246a0ead5854487
Matt Umland <mattumland@gmail.com>
2021-01-14 10:09:52 -0500

    Add missing main.js file

---
+++ player.js
@@ -0,0 +1,25 @@
+class Player {
+  constructor(id) {
+    this.id = id;
+    this.wins = 0;
+    this.hand = [];
+  }
+
+  playCard() {
+/*
+    check length of this.hand to confirm the player has cards
+      remove the first CARD of the hand array
+      return this CARD
+
+      !!!!where should this CARD go!!!!
+
+    blank return when the HAND is empty
+*/
+  }
+
+  saveWinsToStorage() {
+
+  }
+}
+
+module.exports = Player
