cdf06402a618ce248acb77786749b1d3dc6c89ee
Matt Umland <mattumland@gmail.com>
2021-01-17 17:56:15 -0500

    Add functional local storage feature

--- player.js
+++ player.js
@@ -6,29 +6,15 @@ class Player {
   }
 
   drawCard() {
-    // if (this.hand.length > 0) {
       return this.hand.shift();
-    // }
-    // return `empty`;
-
-/*
-    check length of this.hand to confirm the player has cards
-      remove the first CARD of the hand array
-      return this CARD
-
-      this is used to create the variable inside addToCenter
-
-    blank return when the HAND is empty
-*/
   }
 
   saveWinsToStorage() {
-    /*
+    var dataKey = `player${this.id}`;
     var winData = localStorage.getItem('storedWinData');
     var parseWinData = JSON.parse(winData);
-    ADD WINS TO LOCAL STORAGE VARIABLE (WHAT IS THIS?)
+    parseWinData[dataKey]++;
     var stringifyWinData = JSON.stringify(parseWinData);
     localStorage.setItem('storedWinData', stringifyWinData);
-    */
   }
 }
