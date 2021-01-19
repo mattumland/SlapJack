819d1bc125daa601824c37ec1788f7026c7b4868
Matt Umland <mattumland@gmail.com>
2021-01-19 11:34:14 -0500

    Remove comments from game

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
 class Game {
   constructor() {
-    this.player1 = new Player('1'); //should these be instatiated elsewhere?
+    this.player1 = new Player('1');
     this.player2 = new Player('2');
     this.centerPile = [];
-    this.turnTracker = ['1','2']; //default to player1 consider adding a method to randomize starting player
+    this.turnTracker = ['1','2'];
   }
 
   shuffle(deck) {
-      var newDeck = deck.slice(0); //don't change the initial array
-      for (var i = 0; i < newDeck.length; i++) {
+      var newDeck = deck.slice(0);
+      for (var i = 0; i < 100; i++) {
         var pulledCard = newDeck.splice(this.getRandomElement(newDeck),1);
         newDeck.splice(this.getRandomElement(newDeck),0,pulledCard[0]);
       }
@@ -20,9 +20,9 @@ class Game {
   }
 
   initialDeal() {
-    var shufDeck = this.shuffle(fullDeck); //shuffle starting deck
-    this.player1.hand = shufDeck.splice(0,26); //give 1/2 the shuffled deck to p1
-    this.player2.hand = shufDeck.splice(0,26); //give other half of shuffled deck to p2
+    var shufDeck = this.shuffle(fullDeck);
+    this.player1.hand = shufDeck.splice(0,26);
+    this.player2.hand = shufDeck.splice(0,26);
   }
 
   addToCenterPile(currentPlayer, otherPlayer) {
@@ -30,9 +30,9 @@ class Game {
     otherPlayer = `player${otherPlayer}`;
     if (this[currentPlayer].hand.length > 0) {
       var playCard = this[currentPlayer].drawCard();
-      this.centerPile.unshift(playCard); //add new card to the top of centerPile
+      this.centerPile.unshift(playCard);
     }
-    this.changeTurn(); //already has error handling to confirm next player has a hand
+    this.changeTurn();
     if (this[currentPlayer].hand.length === 0 && this[otherPlayer].hand.length === 0) {
       this.addCenterPileToHand(this[currentPlayer]);
     }
@@ -41,30 +41,30 @@ class Game {
   changeTurn() {
     var nextPlayer = `player${this.turnTracker[1]}`;
     if (this[nextPlayer].hand.length > 0) {
-      var turnChanger = this.turnTracker.shift(); //change order of the array
+      var turnChanger = this.turnTracker.shift();
       this.turnTracker.push(turnChanger);
     }
   }
 
   slap(slapPlayer, otherPlayer) {
-    if (this.centerPile[0].includes('jack')) { //check for jack
+    if (this.centerPile[0].includes('jack')) {
       this.addCenterPileToHand(slapPlayer);
       return `SLAPJACK! Player ${slapPlayer.id} takes the pile`;
     }
 
     if (this.centerPile.length === 1){
-    } else if (this.dataCleaner(this.centerPile[0]) === this.dataCleaner(this.centerPile[1])) { //check for double
+    } else if (this.dataCleaner(this.centerPile[0]) === this.dataCleaner(this.centerPile[1])) {
       this.addCenterPileToHand(slapPlayer);
       return `DOUBLES! Player ${slapPlayer.id} takes the pile`;
     }
 
     if (this.centerPile.length === 1 || this.centerPile.length === 2){
-    } else if (this.dataCleaner(this.centerPile[0]) === this.dataCleaner(this.centerPile[2])) { //check for sandwich
+    } else if (this.dataCleaner(this.centerPile[0]) === this.dataCleaner(this.centerPile[2])) {
       this.addCenterPileToHand(slapPlayer);
       return `SANDWICH! Player ${slapPlayer.id} takes the pile`;
     }
 
-    var forfeitCard = slapPlayer.hand.shift();  //assume bad slap without empty hand pass card to other player
+    var forfeitCard = slapPlayer.hand.shift();
     otherPlayer.hand.push(forfeitCard);
     return `BAD SLAP! Player ${slapPlayer.id} loses a card`;
 }
@@ -82,27 +82,27 @@ class Game {
     }
 
     if (this.centerPile[0].includes('jack') && isComeback === false) {
-      this.updateWinCount(slapPlayer); // assume bad slap, update win count for the other player
-      return this.newGame(slapPlayer); //start new game, return winner message
+      this.updateWinCount(slapPlayer);
+      return this.newGame(slapPlayer);
     }
 
     if (!this.centerPile[0].includes('jack') && isComeback === false) {
-      var forfeitCard = slapPlayer.hand.shift();  //assume bad slap without empty hand pass card to other player
+      var forfeitCard = slapPlayer.hand.shift();
       otherPlayer.hand.push(forfeitCard);
       return `BAD SLAP! Player ${slapPlayer.id} loses a card`;
     }
 
     if (!this.centerPile[0].includes('jack') && isComeback === true) {
-      this.updateWinCount(otherPlayer); // assume bad slap, update win count for the other player
-      return this.newGame(otherPlayer); //start new game, return winner message
+      this.updateWinCount(otherPlayer);
+      return this.newGame(otherPlayer);
     }
 }
 
   addCenterPileToHand(player) {
-    var newHand = player.hand.concat(this.centerPile); //join both hands in new var
-    player.hand = newHand; //reassign hand using new var
+    var newHand = player.hand.concat(this.centerPile);
+    player.hand = newHand;
     player.hand = this.shuffle(player.hand);
-    this.centerPile = []; //ensure centerPile is empty
+    this.centerPile = [];
   }
 
   updateWinCount(player) {
@@ -122,5 +122,5 @@ class Game {
     var cleanLink = assetLink.slice(sliceSpot + 1);
     return cleanLink;
   }
-
+  
 }
